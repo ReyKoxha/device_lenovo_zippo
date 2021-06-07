c82386190fd89f16b969e34c3ebae4b87fc495b7
chematelegram <chfondia@gmail.com>
2021-06-07 22:46:41 +0200

    zippo: change qcom-caf HALs

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+rm -rf hardware/qcom-caf/sm8150/display
+git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 11.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+rm -rf hardware/qcom-caf/sm8150/audio
+git clone https://github.com/crdroidandroid/android_hardware_qcom_audio -b 11.0-caf-sm8150 hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media
+git clone https://github.com/crdroidandroid/android_hardware_qcom_media -b 11.0-caf-sm8150 hardware/qcom-caf/sm8150/media
