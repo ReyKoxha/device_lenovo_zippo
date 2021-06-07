4dd3f2388da61c5bd64057c5afa01f659ec362d3
chematelegram <chfondia@gmail.com>
2021-06-07 22:34:49 +0200

    zippo: adapt for PRODUCT BOARD PLATFORM

--- device.mk
+++ device.mk
@@ -9,3 +9,15 @@ $(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
 
 # sm8150-common
 $(call inherit-product, device/lenovo/sm8150-common/common.mk)
+
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msmnile
+TARGET_BOARD_PLATFORM := msmnile
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_FACE_UNLOCK := true
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+
