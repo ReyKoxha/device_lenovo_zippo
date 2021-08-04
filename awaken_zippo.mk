24a701b05de5a817bdca8d4a7e54abd4bc7fb5e8
chematelegram <chfondia@gmail.com>
2021-08-04 14:47:45 +0200

    zippo: Use Redfin fingerprint from August

--- awaken_zippo.mk
+++ awaken_zippo.mk
@@ -23,10 +23,10 @@ PRODUCT_NAME := awaken_zippo
 PRODUCT_GMS_CLIENTID_BASE := android-lenovo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.001 7380771 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys" \
     PRODUCT_NAME="zippo" \
     TARGET_DEVICE="zippo"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210605.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 AWAKEN_BUILD_TYPE := OFFICIAL
